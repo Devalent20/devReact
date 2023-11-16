09767c293f6e480b6dbb2befcf9a97814fe8d788
Shanksus <devalent20@gmail.com>
2023-11-16 15:43:16 +0100

    Array de usuarios

--- projects/01-twitter-card/src/App.jsx
+++ projects/01-twitter-card/src/App.jsx
@@ -3,17 +3,34 @@ import { TwitterFollowCard } from './TwitterFollowCard'
 
 export function App() {
 
+    const users = [{
+        userName: 'midudev',
+        name: 'Miguel Ángel Durán',
+        isFollowing: true,
+    },
+    {
+        userName: 'twitter',
+        name: 'Twitter',
+        isFollowing: false,
+    },
+    {
+        userName: 'juan',
+        name: 'Juan Pérez',
+        isFollowing: false,
+    }]
+
     return (
         <section className='App'>
-            <TwitterFollowCard userName={'midudev'} initialIsFollowing = {true}>
-                Miguel Ángel Durán
-            </TwitterFollowCard>
-            <TwitterFollowCard userName={'twitter'}>
-                Twitter
-            </TwitterFollowCard>
-            <TwitterFollowCard userName={'juan'}>
-                Juan
-            </TwitterFollowCard>
+            {
+                users.map(user => {
+                    const { userName, name, isFollowing } = user
+                    return (
+                        <TwitterFollowCard userName={userName} initialIsFollowing={isFollowing}>
+                            {name}
+                        </TwitterFollowCard>
+                    )
+                })
+            }
         </section>
     )
 }
